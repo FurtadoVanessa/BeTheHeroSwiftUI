97709d856f2ec0766cd30dc293e9cbeaf2e71f2d
Vanessa Furtado <vanessafurtadofagundes@gmail.com>
2020-05-05 15:57:22 -0300

    Initial Commit

---
+++ bethehero/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  bethehero
+//
+//  Created by Douglas castilho on 05/05/20.
+//  Copyright © 2020 Vanessa Furtado. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
